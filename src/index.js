bdb7bc47399a9d9550024aff5f2ac6ed90fb14f6
FlorianB98 <florianbrunet88@gmail.com>
2019-01-07 17:26:18 +0100

    base

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import './css/style.css'
+
+//import image from './images/image.jpg'
+
+//const $img = new Image()
+//$img.src = image
+//document.body.appendChild($img)
+
+import Exemple from './js/Exemple.js'
+
+console.log('Hello Webpack')
+
+const exemple = new Exemple()
+
+if(module.hot)
+{
+    module.hot.accept()
+
+    module.hot.dispose(() =>
+    {
+        console.log('dispose')
+    })
+}
